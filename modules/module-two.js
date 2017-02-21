8acb24f62bf22d517e87426505c5dbf40a512a1a
samvelie <sam.velie@gmail.com>
2017-02-21 14:21:11 -0600

    added semicolon module-two line 4

--- modules/module-two.js
+++ modules/module-two.js
@@ -1,7 +1,7 @@
 function toDollarString(number){
   number = number.toString();
   if(number.length>=4){
-    var lastThree = number.slice(-3)
+    var lastThree = number.slice(-3);
     var firstStuff = number.slice(0, (number.length-3));
     number = '$' + firstStuff + ',' + lastThree;
   }
